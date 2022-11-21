3458f67c4080eacb0f40a2e4c99644968a72c1a0
Silviu <silviu074@gmail.com>
2022-11-21 22:45:33 +0200

    displaying ingredients - css still in progress

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,12 +3,68 @@ import "../styles/Recipe.css"
 
 export const Recipe = ({index,recipe}) => {
 
+    // Enable/Disable pop-up for each recipe
+
     const [popup, setPopup] = useState(false)
 
     const togglePopup = () => {
         setPopup(!popup)
     }
 
+    // Returning the image for an ingredient
+
+    function ingredientImageRender(ingredientsImage, i){
+        let j = eval(`recipe.strIngredient${i}`)
+        if((j !== "") && (j !== 'null')) {
+            let textToSrc = j.replace(" ","%20")
+            ingredientsImage[i] = `https://www.themealdb.com/images/ingredients/${textToSrc}.png`
+            console.log(ingredientsImage[i])
+            return (<img 
+                        className='ingredientImage' 
+                        src={ingredientsImage[i]} 
+                        alt={ingredientsImage[i]}></img>)
+        }
+    }
+
+    // Returning the string value for an ingredient
+
+    function ingredientTextRender(ingredientsText, i){
+        let ingredientText = eval(`recipe.strIngredient${i}`)
+            if((ingredientText !== "") && (ingredientText !== 'null')) {
+                ingredientsText[i] = ingredientText
+                console.log(ingredientsText[i])
+                return (<p>{ingredientsText[i]}</p>)
+            }
+    }
+
+    // Returning the quantity needed for an ingredient
+
+    function measureTextRender(measuresText, i){
+        let measureText = eval(`recipe.strMeasure${i}`)
+        if((measureText !== "") && (measureText !== 'null')){
+            measuresText[i] = measureText
+            console.log(measuresText[i])
+            return (<p>{measuresText[i]}</p>)
+        }
+    }
+
+    // Generating all the ingredients data
+
+    const ingredientsText = []
+    const measuresText = []
+    const ingredientsImage = []
+    const generateIngredients = []
+
+    for(let i=1;i<=20;i++){
+        generateIngredients[i] =
+            <div key={i} className='generatedIngredients'>
+            {ingredientImageRender(ingredientsImage, i)}
+            {ingredientTextRender(ingredientsText, i)}
+            {measureTextRender(measuresText, i)}
+            </div>
+    }
+
+    
 
   return (
     <div key={index} className='render'>
@@ -16,7 +72,10 @@ export const Recipe = ({index,recipe}) => {
             <p>Type of dish: {recipe?.strCategory}</p>
             <p>Recipe origin: {recipe?.strArea}</p>
             {/* <p>{recipe?.strInstructions}</p> */}
-            <img src={recipe?.strMealThumb} className='img'></img>
+            <img 
+                className='img' 
+                src={recipe?.strMealThumb} 
+                alt={recipe?.strMealThumb}></img>
             {/* <p>{recipe?.strYoutube}</p> */}
             <button className='popup-button' onClick={togglePopup}>See recipe</button>
             {popup && (
@@ -26,10 +85,15 @@ export const Recipe = ({index,recipe}) => {
                         <h2>{recipe?.strMeal}</h2>
                         <p>Type of dish: {recipe?.strCategory}</p>
                         <p>Recipe origin: {recipe?.strArea}</p>
-                        <img src={recipe?.strMealThumb} className='img'></img>
+                        <img 
+                            className='img' 
+                            src={recipe?.strMealThumb} 
+                            alt="{recipe?.strMealThumb}"></img>
                         <p>Ingredients:</p>
-                        {recipe.strIngredient1} {recipe.strMeasure1} <br/>
-                        
+
+                        <div className='ingredients'>
+                        {generateIngredients} <br/>
+                        </div>
                     </div>
                 </div>
             ) }
